1fcf5af29d40afff1edcd01cf5c009999ac80eaf
Aman Shinde <1703amanshinde98@gmail.com>
2023-07-26 20:47:10 +0530

    actions & reducers

--- src/component/App.js
+++ src/component/App.js
@@ -1,5 +1,22 @@
-function App() {
-  return <div>Movie App</div>;
-}
+import { Navbar, MovieCard } from "./";
+import { data } from "../data";
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="main">
+        <div className="tabs">
+          <div className="tab">Movie</div>
+          <div className="tab">Favourite</div>
+        </div>
+        <div className="list">
+          {data.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,3 +1,12 @@
-const Navbar = () => {};
+const Navbar = () => {
+  return (
+    <div className="nav">
+      <div className="search-container">
+        <input />
+        <button id="search-btn">Search</button>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
